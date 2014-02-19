2501fe6e8fb103284182800eaf1e9ddb8b2ee8bb
Arel English <arelenglish@gmail.com>
2014-02-19 15:37:22 -0500

    fixed double rspec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,3 @@
 require_relative '../config/environment'
 
 DB[:conn] = SQLite3::Database.new ":memory:"
-
-RSpec.configure do |config|
-  # Use color in STDOUT
-  config.color_enabled = true
-
-  # Use color not only in STDOUT but also in pagers and files
-  config.tty = true
-
-  # Use the specified formatter
-  config.formatter = :progress # :progress, :html, :textmate
-end
